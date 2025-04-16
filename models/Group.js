37314263de2372edbeb3d8b43c1721c2d52e97e9
varunraj1106 <86312464+varunraj1106@users.noreply.github.com>
2025-04-16 12:30:37 +0100

    Update Group.js

--- models/Group.js
+++ models/Group.js
@@ -3,19 +3,7 @@ const mongoose = require('mongoose');
 const groupSchema = new mongoose.Schema({
   name: { type: String, required: true },
   members: [{ type: String, ref: 'User' }],
-  managerId: { type: String, required: true },
-  status: {
-    type: String,
-    enum: [
-      'Requirement Gathering',
-      'In Dev',
-      'Dev Completed',
-      'In Testing',
-      'Testing Done',
-      'Done'
-    ],
-    default: 'Requirement Gathering'
-  }
+  managerId: { type: String, required: true }
 });
 
 module.exports = mongoose.model('Group', groupSchema);
